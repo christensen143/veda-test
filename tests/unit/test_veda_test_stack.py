b69f08a038bbe3de0359c6f678564481ef69f634
Chris Christensen <christopher.christensen@sogeti.com>
2021-11-16 15:48:10 -0500

    Initial commit

---
+++ tests/unit/test_veda_test_stack.py
@@ -0,0 +1,19 @@
+# from aws_cdk import (
+#         core,
+#         assertions
+#     )
+
+# from veda_test.veda_test_stack import VedaTestStack
+
+
+# example tests. To run these tests, uncomment this file along with the example
+# resource in veda_test/veda_test_stack.py
+def test_sqs_queue_created():
+#     app = core.App()
+#     stack = VedaTestStack(app, "veda-test")
+#     template = assertions.Template.from_stack(stack)
+
+#     template.has_resource_properties("AWS::SQS::Queue", {
+#         "VisibilityTimeout": 300
+#     })
+  pass
